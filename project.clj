5382957dc58203edea2df76d20034de079a7b470
Gustavo Villalta <gustavo.villalta@nubank.com.br>
2018-11-24 18:29:23 -0200

    Organize the project

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://www.nubank.com.br"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [adamwynne/feedparser-clj "0.5.2"]
-                 [org.clojure/tools.reader "1.3.2"]
-                 [commons-validator "1.5.1"]
-                 [com.novemberain/pantomime "2.10.0"]
-                 [remus "0.1.0-SNAPSHOT"]
+  :dependencies [[adamwynne/feedparser-clj "0.5.2"]
                  [clj-http "3.9.1"]
+                 [com.novemberain/pantomime "2.10.0"]
+                 [commons-validator "1.5.1"]
+                 [digest "1.4.8"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.reader "1.3.2"]
                  [overtone/at-at "1.2.0"]
-                 [digest "1.4.8"]]
+                 [remus "0.1.0-SNAPSHOT"]]
   :jvm-opts ["-Xmx4g" "-server"]
   :main ^:skip-aot readerss.core
   :target-path "target/%s"
